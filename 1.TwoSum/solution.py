e98acfa49cdf6069dcb3b0ad29f3b6fc03e4c6ad
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-26 11:46:38 +0000

    Added initial commit for the 1.TwoSum problem.

---
+++ 1.TwoSum/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """Given an array of integers, return indices of the two numbers 
+        such that they add up to a specific target. You may assume that 
+        each input would have exactly one solution, and you may not use 
+        the same element twice
+        
+        Arguments:
+            nums {List[int]} -- List of integers
+            target {int} -- The target for the addition
+        
+        Returns:
+            List[int] -- List with the indices of the two numbers that 
+            add up to the target
+        """
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                pass
+
+        return 1
+
+        
