94afea614a09a567c6342c40de2f00adc8a9e1de
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-02-28 14:45:38 +0000

    Added a simple implemenation of a node for a singly-linked list.

---
+++ LinkedList/singlyLinkedNode.py
@@ -0,0 +1,13 @@
+class SinglyLinkedNode():
+    """Implements a singly-linked node.
+    """
+
+    value = None
+    nextNode = None
+
+    def setvalue(self, x):
+        value = x
+
+    def setNextNode(self, node):
+        nextNode = node
+    
