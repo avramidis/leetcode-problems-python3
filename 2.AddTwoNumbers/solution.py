4f84f019b047feed192d35dcf783f7cefcf33b53
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-03-01 14:29:06 +0000

    Added code for the add two numbers problem.

---
+++ 2.AddTwoNumbers/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """You are given two non-empty linked lists representing two non-negative integers. 
+        The digits are stored in reverse order and each of their nodes contain a single digit. 
+        Add the two numbers and return it as a linked list. You may assume the two numbers 
+        do not contain any leading zero, except the number 0 itself.
+        
+        Arguments:
+            l1 {[type]} -- [description]
+            l2 {[type]} -- [description]
+        """
+
+        pass
